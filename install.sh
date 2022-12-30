d2d1610a9d4c284b4085b8e9d192125bf21bffae
Zachary Snow <zachary.snow@gmail.com>
2022-12-30 21:33:53 +0100

    Add powerlevel10k

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# Install
 sudo apt install -y \
   git \
   stow \
@@ -5,9 +6,14 @@ sudo apt install -y \
   wget \
   zsh
 
+# Add configuration symlinks
 stow git
+stow powerlevel10k
 stow zsh
 
 # Add zsh as a login shell and set as default shell
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER
+
+# powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
