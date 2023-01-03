7bfa570849921d88908cbdef415e033aeb26117d
Zachary Snow <zachary.snow@gmail.com>
2023-01-03 08:57:36 +0100

    Adding keybindings

--- install-git-bash.sh
+++ install-git-bash.sh
@@ -1,8 +1,8 @@
 # See https://gist.github.com/fworks/af4c896c9de47d827d4caa6fd7154b6b to install zsh
 
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/powerlevel10k/.p10k.zsh ~/.p10k.zsh
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+# Update ~/.gitconfig to include ~/.dotfiles/git/.gitconfig
+echo "source ~/.dotfiles/powerlevel10k/.p10k.zsh" > ~/.p10k.zsh
+echo "source ~/.dotfiles/zsh/.zshrc" > ~/.zshrc
 
 mkdir -p ~/.config/git
 echo "" > ~/.config/git/git-prompt.sh
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -48,3 +48,12 @@ source ~/.dotfiles/terminal/init.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Keybindings
+bindkey "\e[1;5D" backward-word
+bindkey "\e[1;5C" forward-word
+bindkey "\e[3;5~" kill-word
+bindkey "\C-_"    backward-kill-word
+bindkey "\e[3~" delete-char
+bindkey "\e[H"  beginning-of-line
+bindkey "\e[F"  end-of-line
