2f6720e685cf37ace7f9ebe640593227c5496379
Zachary Snow <zachary.snow@gmail.com>
2023-03-21 23:21:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,8 @@
-__SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd $__SCRIPTDIR
-
 # Install
 source ~/.dotfiles/install-packages.sh
 
 # Add configuration symlinks
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
-# Add zsh as a login shell and set as default shell
-command -v zsh | sudo tee -a /etc/shells
-sudo chsh -s $(which zsh) $USER
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm ~/.zshrc
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-
-popd
-unset __SCRIPTDIR
+# Link .bashrc
+echo "source ~/.dotfiles/bash/.bashrc" > ~/.bash_profile
