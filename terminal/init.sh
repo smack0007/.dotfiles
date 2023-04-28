7d7f7619571051e6ed586fc4167b8a5d4e79e98e
Zachary Snow <zachary.snow@gmail.com>
2023-04-28 19:58:35 +0200

    Remove nvm stuff and add node prefix

--- terminal/init.sh
+++ terminal/init.sh
@@ -13,9 +13,8 @@ if is_wsl; then
   PATH=$(sed ':a; N; $!ba; s/\n/:/g' ~/.dotfiles/terminal/wsl.path)
 fi
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export NODE_PREFIX="$HOME/.node"
+export PATH="$NODE_PREFIX/bin:$PATH"
 
 export DENO_INSTALL="$HOME/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
