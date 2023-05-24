ffc794fb1e3fb499f53c202ad9456f17133b5ba4
Zachary Snow <zachary.snow@gmail.com>
2023-05-24 12:12:33 +0200

    Update git bash instructions

--- install-git-bash.sh
+++ install-git-bash.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Link .gitconfig
 git config --global include.path "$(cygpath --absolute --long-name --windows ~/.dotfiles/git/.gitconfig)"
 
 # Link .bashrc
 echo "source ~/.dotfiles/bash/.bashrc" > ~/.bash_profile
-
-# Link .zshrc
-# echo "source ~/.dotfiles/zsh/.zshrc" > ~/.zshrc
 
 # Get rid of error related to being unable to load bash completions
 mkdir -p ~/.config/git
