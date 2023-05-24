5410555c525c0546c0c5b1dc8110bcbe04e153d9
Zachary Snow <zachary.snow@gmail.com>
2023-05-24 14:17:18 +0200

    Add directory checks to known tools

--- terminal/init.sh
+++ terminal/init.sh
@@ -13,10 +13,20 @@ if is_wsl; then
   PATH=$(sed ':a; N; $!ba; s/\n/:/g' ~/.dotfiles/terminal/wsl.path)
 fi
 
-export NODE_PREFIX="$HOME/.node"
-export PATH="$NODE_PREFIX/bin:$PATH"
+if [ -d "$HOME/.node" ]; then
+  export NODE_PREFIX="$HOME/.node"
+  export PATH="$NODE_PREFIX/bin:$PATH"
+fi
 
-export DENO_INSTALL="$HOME/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
+if [ -d "$HOME/.node" ]; then
+  export DENO_INSTALL="$HOME/.deno"
+  export PATH="$DENO_INSTALL/bin:$PATH"
+fi
 
 [ -f ~/.ssh/github_ed25519 ] && eval $(ssh-agent) &>/dev/null && ssh-add ~/.ssh/github_ed25519 &>/dev/null
