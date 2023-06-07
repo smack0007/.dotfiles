ea073419335cb1f361c22b848387ec444aa7ff32
Zachary Snow <zachary.snow@gmail.com>
2023-06-07 20:50:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 # Install
-source ~/.dotfiles/install-packages.sh
+# source ~/.dotfiles/install-packages.sh
 
 # Add configuration symlinks
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+git config --global include.path "~/.dotfiles/git/.gitconfig"
 
 # Link .bashrc
 echo "source ~/.dotfiles/bash/.bashrc" > ~/.bash_profile
+echo "source ~/.dotfiles/bash/.bashrc" > ~/.bashrc
