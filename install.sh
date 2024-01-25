508cd2c018aea8bb43b00e770ff4e3e50b86754a
Zachary Snow <zachary.snow@gmail.com>
2024-01-25 14:42:24 +0100

    Update install.sh to install for zsh

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ git config --global include.path "~/.dotfiles/git/.gitconfig"
 # Link .bashrc
 echo "source ~/.dotfiles/bash/.bashrc" > ~/.bash_profile
 echo "source ~/.dotfiles/bash/.bashrc" > ~/.bashrc
+
+# Link .zshrc
+echo "source ~/.dotfiles/zsh/.zshrc" > ~/.zprofile
+echo "source ~/.dotfiles/zsh/.zshrc" > ~/.zshrc
