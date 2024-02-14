9ce50c39d895e9a188bed0271f8b627c7db177d1
Zachary Snow <zachary.snow@gmail.com>
2024-02-14 22:58:53 +0100

    Added alacritty config

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 # Add configuration symlinks
 git config --global include.path "~/.dotfiles/git/.gitconfig"
 
+# Link alacritty config
+mkdir -p ~/.config/alacritty && echo 'import = [ "~/.dotfiles/.config/alacritty/alacritty.toml" ]' > ~/.config/alacritty/alacritty.toml
+
 # Link .bashrc
 echo "source ~/.dotfiles/bash/.bashrc" > ~/.bash_profile
 echo "source ~/.dotfiles/bash/.bashrc" > ~/.bashrc
